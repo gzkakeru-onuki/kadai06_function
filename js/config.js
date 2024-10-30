4e72c99139262f222fcc564b277c7d39ffe80065
gzkakeru-onuki <gzdevonuki@gmail.com>
2024-10-30 17:52:38 +0900

    first commit

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDQikdgYczA6Oupx3QDnh0UDLsf4IHESW4",
+    authDomain: "ippon-a9de8.firebaseapp.com",
+    databaseURL: "https://ippon-a9de8-default-rtdb.firebaseio.com",
+    projectId: "ippon-a9de8",
+    storageBucket: "ippon-a9de8.appspot.com",
+    messagingSenderId: "264615532603",
+    appId: "1:264615532603:web:2a98e858ed9031cfa5e41e"
+};
+
+export{firebaseConfig};
